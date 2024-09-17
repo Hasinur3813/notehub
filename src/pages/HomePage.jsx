9857b62062a78c5e2a59a29fa65cbbbd94c58e9f
Hasinur <hasinur.rahman1332@gmail.com>
2024-09-17 21:48:51 +0600

    second commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,9 +5,9 @@ import { Button } from "../components/Button";
 const HomePage = () => {
   return (
     <section className="overflow-y-auto w-full py-8 md:py-0 bg-gradient-to-r from-indigo-500 to-sky-500 px-4">
-      <div className="container mx-auto flex flex-col md:flex-row text-center md:text-left justify-center md:justify-between gap-y-10 items-center h-full">
-        <div class=" text-white rounded mb-8">
-          <h1 class="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">
+      <div className="container mx-auto flex flex-col md:flex-row text-center md:text-left  md:justify-between gap-y-10 items-center h-full">
+        <div className=" text-white rounded mb-8">
+          <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">
             Welcome to NoteHub
           </h1>
           <p className="my-5 leading-relaxed md:max-w-lg text-base">
